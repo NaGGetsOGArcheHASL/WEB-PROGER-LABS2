d428f8dfd07f4f0e8cf746610cab741c793ae0ba
Максим Нагайцев <mcnaggets69@mail.ru>
2023-09-29 16:50:49 +0700

    Вывод первой страницы лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,25 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Нагайцев Максим Алексеевич, Лабораторная работа 1</title>
+</head>
+<body>
+    <header>
+        НГТУ, ФБ, Лабораторная работа 1
+    </header>
+
+    <h1> WEB-сервер на Flask</h1>
+
+    <footer>
+        &copy; Нагайцев Максим, ФБИ-12, 3 курс, 2023
+    </footer>
+    
+</body>
+</html>
+"""
